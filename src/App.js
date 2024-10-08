ea9cd8f3f842a6053e5d0504aab6028a1bc60d5e
Carmel-Abigail-Shirley-Royan <carmelabigailroyan@example.com>
2024-10-08 22:23:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './styles.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTask = () => {
+    if (inputValue.trim()) {
+      setTasks([...tasks, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+  const Delete = (index) => {
+    const arr = [];
+    for (let i = 0; i < tasks.length; i++) {
+      if (i !== index) {
+        arr.push(tasks[i]);
+      }
+    }
+    setTasks(arr);
+  }
+  const Display = ({ task, index }) => {
+    return (<div ><h3 className="task-box">{task} </h3>
+      <button onClick={() => Delete(index)} className='del text2'>Delete</button>
+    </div>);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>To Do List</h1>
+      <div className='text'>
+        <input
+          type='text'
+          id="ans"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className='my-button' onClick={addTask}>Add Task</button>
+      </div>
+      <div className='text2'>
+        {tasks.map((task, index) => (
+          <Display key={index} task={task} index={index} />
+        ))}
+      </div>
     </div>
   );
 }
